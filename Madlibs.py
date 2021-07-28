dadd9daaacd041845ba3ce7f79224614064f8cde
Zayd-s <zaydsabeh@hotmail.com>
2021-07-29 00:12:31 +0100

    added simple madlibs project

---
+++ Madlibs.py
@@ -0,0 +1,10 @@
+name1 = input("Type in a name: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+adjective1 = input("Adjective: ")
+
+madlib = f"There was once a coder named + {name1} and he really liked to {verb1}.\
+\nHe also really liked to {verb2}, but he never liked to {verb3}, because it was so {adjective1}"
+
+print(madlib)
